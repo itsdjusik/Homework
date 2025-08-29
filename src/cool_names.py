9ee7582d7e92e63455a0d255b67be39af27a0074
Дмитрий Зенкин <ka2666113@gmail.com>
2025-08-30 02:47:38 +1100

    add func get_only_names

--- src/cool_names.py
+++ src/cool_names.py
@@ -6,7 +6,7 @@ def get_only_names(file_names: str) -> list[str]:
     with open(file_names, "r", encoding="utf-8") as file:
       for line in file:
         line = line.strip()
-        only_name = " ".join(c for c in line if c.isalpha() or c.isspace())
+        only_name = "".join(c for c in line if c.isalpha())
         if only_name:
           cool_names.append(only_name)
     return cool_names
@@ -14,5 +14,5 @@ def get_only_names(file_names: str) -> list[str]:
     print("Файл не найден! Проверьте путь к файлу!")
     return []
 if __name__ == "__main__":
-    result = get_only_names("names.txt")
+    result = get_only_names("data/names.txt")
     print(*result, sep="\n")
