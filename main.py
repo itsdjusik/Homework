3b31d1c71083c2a087946721042733ab3c41b5fe
Дмитрий Зенкин <ka2666113@gmail.com>
2025-09-02 21:44:33 +1100

    import func create_name_filter, path_name_filter

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from src.clean_names import get_only_names
-from src.clean_names import writing_file, file_path
+#from src.clean_names import writing_file, file_path
+from src.name_filter import create_name_filter, path_name_filter
+
 
 result = get_only_names("src/data/names.txt")
 print(*result, sep="\n")
-writing_file(result, file_path)
+#writing_file(result, file_path)
+create_name_filter(path_name_filter)
