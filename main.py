0dcce83e840c2de81bcd6eac67c0c59bed07abb5
Дмитрий Зенкин <ka2666113@gmail.com>
2025-09-02 19:52:04 +1100

    add import writing_file, file_path

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from src.cool_names import get_only_names
+from src.cool_names import writing_file, file_path
 
-result = get_only_names("src/data/name_test_file")
+result = get_only_names("src/data/names.txt")
 print(*result, sep="\n")
+writing_file(result, file_path)
