de60d3b23122bde685fe645998e3a824c34d949d
Дмитрий Зенкин <ka2666113@gmail.com>
2025-09-02 21:04:22 +1100

    create func create_name_filter

---
+++ src/name_filter.py
@@ -0,0 +1,29 @@
+import os
+import re
+
+
+def create_name_filter(file_names: str) -> tuple[list[str], list[str]]:
+    """Принимает файл с именами на разном языке, возвращает два файла с именами на русском
+    и английском языке"""
+
+    eng_names = []
+    rus_names = []
+
+    if os.path.exists("src/data"):
+        try:
+            with open(file_names, "r", encoding="utf-8") as file:
+                for name in file:
+                    name = name.strip()
+                    if re.fullmatch(r"[A-Za-z]+", name):
+                        eng_names.append(name)
+                    if re.fullmatch(r"[А-Яа-яЁё]+", name):
+                        rus_names.append(name)
+            eng_names.sort()
+            rus_names.sort()
+            return eng_names, rus_names
+        except FileNotFoundError:
+            print("Файл отсутствует!")
+    else:
+        os.makedirs("src/data")
+
+
