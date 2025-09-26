63b3e818b30dc33393089b9e1e84e553c656c444
Дмитрий Зенкин <ka2666113@gmail.com>
2025-09-26 16:28:29 +1100

    add func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -10,4 +10,17 @@ data_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.
              {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
              {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
+
 print(*filter_by_state(data_list), sep="\n")
+
+
+def sort_by_date(data_rev: list[dict[str, str]], reverse: bool = True) -> list[dict[str, str]]:
+  """Функция принимает список словарей и необязательный
+  параметр, задающий порядок сортировки (по умолчанию — убывание)"""
+
+  new_data_rev = list(sorted(data_rev, key=lambda x: x['date'], reverse=reverse))
+  return new_data_rev
+
+
+print(*sort_by_date(data_list), sep="\n")
+
